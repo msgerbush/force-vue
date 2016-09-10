dee59f4b85523f561baf6ce6802b9d633c0a244f
Michael Gerbush <michaelg@spiceworks.com>
2016-09-10 17:26:36 -0400

    Cleanup ESLint issues.
    Remove duplicate .eslintrc, since we’re using airbnb’s.
    Update eslintignore to ignore build and conf files from vue template.
    Update config files to give us multiple environments based on latest vue
    template for web pack.
    Update to latest airbnb linter.
    Fix linting issues.
    Ignore requiring devDependencies in the test folder.
    Add eslint-import-resolver-webpack to handle linting with webpack 
    aliases.

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,13 +1,21 @@
 module.exports = {
   root: true,
-  extends: 'airbnb/base',
   // required to lint *.vue files
   plugins: [
     'html',
   ],
+  extends: 'airbnb-base',
   // add your custom rules here
   rules: {
     // allow debugger during development
     'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0,
+  },
+  settings: {
+    'import/resolver': {
+      'webpack': {
+        'config': process.env.NODE_ENV === 'testing'
+          ? 'build/webpack.prod.conf.js' : 'build/webpack.dev.conf.js'
+      }
+    }
   },
 };
