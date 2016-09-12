eabf28988f9fd3faf7bb8f6a91b9f90e17f2de54
Michael Gerbush <michaelg@spiceworks.com>
2016-09-11 20:52:26 -0400

    Fix env files from busted merge.

--- config/dev.env.js
+++ config/dev.env.js
@@ -2,9 +2,5 @@ const merge = require('webpack-merge');
 const prodEnv = require('./prod.env');
 
 module.exports = merge(prodEnv, {
-<<<<<<< HEAD
-  NODE_ENV: '"development"'
-=======
   NODE_ENV: '"development"',
->>>>>>> add-environment-support
 });
